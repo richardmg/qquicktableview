7b3dac3dbeff62b16d2bd0e8cd418fff220f498b
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2019-05-02 10:26:48 +0200

    implement updateExtents()
    Change-Id: I6060b7e84b9489c8fa569e6ff41b958e3871f8e7

--- src/quick/items/qquicktableview_p.h
+++ src/quick/items/qquicktableview_p.h
@@ -147,6 +147,9 @@ private:
     Q_DISABLE_COPY(QQuickTableView)
     Q_DECLARE_PRIVATE(QQuickTableView)
 
+    qreal minXExtent() const override;
+    qreal maxXExtent() const override;
+
     Q_PRIVATE_SLOT(d_func(), void _q_componentFinalized())
 };
 
